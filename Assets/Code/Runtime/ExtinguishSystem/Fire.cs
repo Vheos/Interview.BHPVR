1cd618549e31f908bbfcfe084922b65fde4ac413
Vheos <Vheos777@gmail.com>
2024-02-03 17:38:18 +0100

    Added scaling with health

--- Assets/Code/Runtime/ExtinguishSystem/Fire.cs
+++ Assets/Code/Runtime/ExtinguishSystem/Fire.cs
@@ -7,9 +7,9 @@ namespace Vheos.Interview.BHPVR
 		// Dependencies
 		[field: SerializeField] public ParticleSystem ParticleSystem { get; private set; }
 		[field: SerializeField] public Layer ExtinguishingLayer { get; private set; }
-		[field: SerializeField] public Rigidbody Rigidbody { get; private set; }
-		[field: SerializeField, Range(0f, 30f)] public float StartingHealth { get; private set; }
+		[field: SerializeField, Range(1f, 30f)] public float MaxHealth { get; private set; }
 		[field: SerializeField, Range(0f, 5f)] public float HealthRegen { get; private set; }
+		[field: SerializeField] public AnimationCurve SizeByHealth { get; private set; }
 
 		// Fields
 		private float health;
@@ -17,23 +17,23 @@ namespace Vheos.Interview.BHPVR
 		private bool hasBeenSprayedThisFrame;
 
 		// Methods
+		public bool IsBurning
+			=> ParticleSystem.isEmitting;
 		public float Health
 		{
 			get => health;
 			private set
 			{
-				if (value == health)
-					return;
+				health = Mathf.Clamp(value, 0f, MaxHealth);
+				UpdateSize();
 
-				health = value;
-
-				if (health <= 0f)
-					Destroy(gameObject);
+				if (health == 0f)
+					ParticleSystem.Stop();
 			}
 		}
 		private void Initialize()
 		{
-			health = StartingHealth;
+			Health = MaxHealth;
 			extinguishingLayerMask = ExtinguishingLayer.ToLayerMask();
 		}
 		private void TryGetSprayed(Collider other)
@@ -46,8 +46,13 @@ namespace Vheos.Interview.BHPVR
 		}
 		private void TryRegenHealth()
 		{
-			if (!hasBeenSprayedThisFrame && health < StartingHealth)
+			if (IsBurning && !hasBeenSprayedThisFrame)
 				Health += HealthRegen * Time.fixedDeltaTime;
+		}
+		private void UpdateSize()
+		{
+			float size = SizeByHealth.Evaluate(health / MaxHealth);
+			ParticleSystem.transform.localScale = new(size, size, size);
 		}
 
 		// Unity
